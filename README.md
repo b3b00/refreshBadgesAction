ea67786c6b5c8d2fdbdcdc9336fcc9375b13f221
Olivier Duhart <olivier.duhart@sage.com>
2022-07-01 13:34:06 +0200

    initial

---
+++ README.md
@@ -0,0 +1,21 @@
+# Coverlet action
+
+This action runs [coverlet coverage](https://github.com/coverlet-coverage/coverlet) tool on a dotnet core solution
+
+
+
+## Inputs
+
+### `repository`
+
+**Required** the name of the repository where to refresh badges, using format <user>/<repository>. ex: `b3b00/refreshBadgesActions` for this repository
+
+### `branch`
+
+**Optional** the name of the branch to refresh (default to `main`). ex : `develop` 
+
+
+
+## Outputs
+
+ no outputs
---
+++ index.js
@@ -0,0 +1,50 @@
+const core = require("@actions/core");
+//const core = require("../JsGithubActionEmulator/core");
+const github = require("@actions/github");
+const { exec, execSync } = require("child_process");
+const path = require("path");
+const request = require("request");
+var fs = require("fs");
+var cheerio = require("cheerio");
+
+function ab2str(buf) {
+  return String.fromCharCode.apply(null, buf);
+}
+
+try {
+  const repository = core.getInput("repository");
+  let branch = core.getInput("branch");
+  if (!branch) {
+    branch = "main";
+  }
+
+  const readmeUrl = `https://github.com/${repository}/blob/${branch}/README.md`;
+
+  request(readmeUrl, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      let content = cheerio.load(response.body);
+
+      content("img[data-canonical-src]").each(function (i, elem) {
+        const cacheSrc = elem["attribs"]["src"];
+        const badgeUrl = elem["attribs"]["data-canonical-src"];
+        request(
+          cacheSrc,
+          { method: "PURGE" },
+          function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+              console.log(`cached purged for ${badgeUrl}`);
+            } else {
+              console.log(`error while purging badge ${badgeUrl}: ${error}`);
+            }
+          }
+        );
+      });
+    } else {
+      console(`readme fetching failed wih : ${error.message}`);
+    }
+  }).pipe(fs.createWriteStream(`${repository.replace("/", "_")}.md`));
+} catch (error) {
+  console.log("global error " + error);
+  console.log(error.stack);
+  core.setFailed(error.message);
+}
